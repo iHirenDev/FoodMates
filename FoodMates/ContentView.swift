67e8bec4290f4cfa8245cccc3b0bc4a3aded44bb
Hiren <patelhiren9857@gmail.com>
2020-07-25 16:21:48 +1000

    Initial Commit

---
+++ FoodMates/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodMates
+//
+//  Created by Hiren Patel on 25/7/20.
+//  Copyright © 2020 com.hiren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodMatesTests/FoodMatesTests.swift
@@ -0,0 +1,34 @@
+//
+//  FoodMatesTests.swift
+//  FoodMatesTests
+//
+//  Created by Hiren Patel on 25/7/20.
+//  Copyright © 2020 com.hiren. All rights reserved.
+//
+
+import XCTest
+@testable import FoodMates
+
+class FoodMatesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
